321f5d164a6278dad1c7f2f7e17395f7bd062f3c
Maxim Dankovtsev <max.dankow@gmail.com>
2016-03-06 12:33:29 +0300

    Add research results.

--- Futex/main.cpp
+++ Futex/main.cpp
@@ -3,11 +3,14 @@
 #include <vector>
 #include <iostream>
 #include <mutex>
+#include <chrono>
+#include <assert.h>
 
 using std::thread;
 using std::atomic;
 using std::vector;
 using std::cout;
+using std::mutex;
 
 class Futex {
 public:
@@ -39,31 +42,31 @@ private:
 };
 
 const size_t THREAD_NUMBER = 4;
-const int MAX_GLOBAL = 10000000;
+const int MAX_GLOBAL = 100000000;
 
-void incrementer(Futex &futex, size_t &count, int &value, int maxValue) {
+void incrementer(mutex &mutex, size_t &count, int &value, int maxValue) {
     while (true) {
-        futex.lock();
+        mutex.lock();
         if (value < maxValue) {
             value++;
             ++count;
         } else {
-            futex.unlock();
+            mutex.unlock();
             break;
         }
-        futex.unlock();
+        mutex.unlock();
     }
 }
 
 size_t threadsCountToN(size_t threadNumber, int maxValue) {
     vector<std::thread> threads;
     vector<size_t> results(threadNumber);
-    Futex futex;
+    mutex mutex;
     int global = 0;
 
     for (size_t i = 0; i < threadNumber; ++i) {
         threads.push_back(
-                std::thread(incrementer, std::ref(futex), std::ref(results[i]), std::ref(global), maxValue)
+                std::thread(incrementer, std::ref(mutex), std::ref(results[i]), std::ref(global), maxValue)
         );
     }
 
@@ -77,7 +80,31 @@ size_t threadsCountToN(size_t threadNumber, int maxValue) {
     return operationCount;
 }
 
+std::chrono::milliseconds measureSingleRun(size_t threadNumber) {
+    auto timeStart = std::chrono::steady_clock::now();
+
+    size_t  operationCount = threadsCountToN(threadNumber, MAX_GLOBAL);
+
+    auto timeStop = std::chrono::steady_clock::now();
+    auto workTime = std::chrono::milliseconds(std::chrono::duration_cast<std::chrono::milliseconds>(timeStop - timeStart).count());
+    cout << threadNumber << " threads " << operationCount << " in " << workTime.count() << "ms." << std::endl;
+    return workTime;
+}
+
+std::chrono::milliseconds measureAverageTime(size_t threadNumber, size_t count) {
+    assert(count > 0);
+    std::chrono::milliseconds sum(0);
+    for (size_t i = 0; i < count; ++i) {
+        sum += measureSingleRun(threadNumber);
+    }
+    auto averageTime = sum / count;
+    cout << "Average time for " << threadNumber << " threads is " << averageTime.count() << std::endl;
+}
+
 int main() {
-    threadsCountToN(THREAD_NUMBER, MAX_GLOBAL);
+    measureAverageTime(thread::hardware_concurrency() / 4, 10); // 7601 ms (4275 ms)
+    measureAverageTime(thread::hardware_concurrency() / 2, 10); // 17724 ms (21920 ms)
+    measureAverageTime(thread::hardware_concurrency(), 5); // 28691 ms (18039)
+    measureAverageTime(thread::hardware_concurrency() * 2, 3); // 53792 ms (20773 ms)
     return 0;
 }
