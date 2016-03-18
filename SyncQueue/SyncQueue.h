4ef49c7b2e6a9b2295d23868106f2460b228ea83
Maxim Dankovtsev <max.dankow@gmail.com>
2016-03-18 23:54:56 +0300

    Add SQueue.close(), general test works.

--- SyncQueue/SyncQueue.h
+++ SyncQueue/SyncQueue.h
@@ -1,6 +1,7 @@
 #ifndef SYNCQUEUE_SYNCQUEUE_H
 #define SYNCQUEUE_SYNCQUEUE_H
 
+#include <exception>
 #include <mutex>
 #include <condition_variable>
 #include "Optional.h"
@@ -19,40 +20,62 @@
 template<class C>
 class SyncQueue {
 public:
+
+    explicit SyncQueue() : closed(false) { }
+
     typedef typename C::value_type ValueType;
 
     // Помещает элемент в очередь в любом случае.
     void push(const ValueType &element);
 
     // Извлекает элемент их очереди, если она не пуста, в противном случае поток блокируется.
-    ValueType& popOrWait();
-    Optional<typename C::value_type> popNoWait();
+    Optional<ValueType> popOrWait();
+
+    // Извлекает элемент из очереди если очередь не пуста.
+    Optional<ValueType> popNoWait();
+
+    void close();
+
+    bool isClosed() const {
+        return closed;
+    }
 
 private:
     C data;
     std::mutex mutex;
     std::condition_variable isEmpty;
+    bool closed;
 };
 
 
 template<class C>
-void SyncQueue<C>::push(const typename C::value_type &element) {
+void SyncQueue<C>::push(const SyncQueue::ValueType &element) {
     std::unique_lock<std::mutex> locker(mutex);
+    if (closed) {
+        throw std::logic_error("Queue is closed");  // Деструктор locker вызовется и mutex раблокируется
+    }
     data.push(element);
     isEmpty.notify_all();
     // В деструкторе unique_lock делает unlock, если текущий поток владеет мьютексом.
 }
 
 template<class C>
-typename C::value_type& SyncQueue<C>::popOrWait() {
+Optional<typename C::value_type> SyncQueue<C>::popOrWait() {
     std::unique_lock<std::mutex> locker(mutex);
-    // Ждем пока нет элементов в очереди.
-    while (data.empty()) {
+
+    // Ждем пока нет элементов в очереди и есть что ждать (очередь не закрыта).
+    while (data.empty() && !closed) {
         isEmpty.wait(locker);
     }
+
+    // В этот момент не может быть открытой и одновременно пустой очереди.
+    if (closed && data.empty()) {
+        return Optional<SyncQueue::ValueType > ();  // None
+    }
+
     auto element = data.front();
     data.pop();
-    return element;
+    return Optional<SyncQueue::ValueType > (element);  // Some(element)
     // В деструкторе unique_lock делает unlock, если текущий поток владеет мьютексом.
 }
 
@@ -62,7 +85,7 @@ Optional<typename C::value_type> SyncQueue<C>::popNoWait() {
     mutex.lock();
     if (data.empty()) {
         mutex.unlock();
-        return Optional<typename C::value_type>();  // Вернуть None.
+        return Optional<typename C::value_type>();  // Вернуть None
     } else {
         auto element = data.front();
         data.pop();
@@ -70,5 +93,17 @@ Optional<typename C::value_type> SyncQueue<C>::popNoWait() {
         return Optional<typename C::value_type>(element);  // Вернуть Some(element)
     }
 }
+
+template <class C>
+void SyncQueue<C>::close() {
+    std::unique_lock<std::mutex> locker(mutex);
+    if (closed) {
+        throw std::logic_error("Queue is already closed");  // Деструктор locker вызовется и mutex раблокируется
+    }
+    closed = true;
+    isEmpty.notify_all();
+    locker.unlock();
+}
+
 
 #endif //SYNCQUEUE_SYNCQUEUE_H
--- SyncQueue/Tests/Tests.cpp
+++ SyncQueue/Tests/Tests.cpp
@@ -2,9 +2,10 @@
 #include <queue>
 #include <stack>
 #include <list>
-#include <set>
-#include "../Optional.h"
+#include <thread>
+#include "../SyncQueue.h"
 #include "../Detector.h"
+#include <algorithm>
 
 TEST(Detector, HaveMethods) {
     // hasPush
@@ -57,6 +58,68 @@ TEST(Detector, Unsuitability) {
     ASSERT_FALSE(Detector<std::set<Optional<int> > >::pushBackPopBackSuitable);
 }
 
+template<class C>
+void worker(std::vector<int> &values, SyncQueue<C> &taskQueue, std::mutex &mutex) {
+    while (true) {
+        Optional<size_t> optionalTask = taskQueue.popOrWait();
+        size_t index;
+        if (optionalTask.some(index)) {
+            mutex.lock();
+            values[index]++;
+            mutex.unlock();
+        } else {
+            break;
+        }
+    }
+}
+
+auto engine = std::default_random_engine{};
+
+template<class C>
+bool master(size_t threadNumber) {
+    const size_t DATA_SIZE = 10000;
+    const int VALUE = 50;
+    std::vector<int> values(DATA_SIZE);
+    SyncQueue<C> syncQueue;
+    std::mutex mutex;
+    std::vector<std::thread> threads;
+
+    for (size_t i = 0; i < threadNumber; ++i) {
+        threads.push_back(
+                std::thread(worker<C>, std::ref(values), std::ref(syncQueue), std::ref(mutex)));
+    }
+
+    // Формируем задачи.
+    std::vector<size_t> tasks;
+    tasks.reserve(DATA_SIZE);
+    for (size_t i = 0; i < DATA_SIZE; ++i) {
+        tasks.push_back(i);
+    }
+    std::shuffle(tasks.begin(), tasks.end(), engine);
+
+    for (size_t task : tasks) {
+        for (int i = 0; i < VALUE; ++i) {
+            syncQueue.push(task);
+        }
+    }
+    syncQueue.close();
+
+    for (size_t i = 0; i < threads.size(); ++i) {
+        threads[i].join();
+    }
+
+    // Проверяем результат.
+    for (int i : values) {
+        if (i != VALUE) {
+            return false;
+        }
+    }
+    return true;
+}
+
+TEST(SyncQueue, General) {
+    ASSERT_TRUE(master<std::queue<size_t >>(2));
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
