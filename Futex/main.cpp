7d6355b901ffd79b28f1aef66b2a8a8b33f482ed
Maxim Dankovtsev <max.dankow@gmail.com>
2016-05-05 23:51:00 +0300

    Futex fixed

--- Futex/main.cpp
+++ Futex/main.cpp
@@ -23,16 +23,15 @@ public:
 
     void lock() {
         int desired = NO_OWNER;
-        while (!owner.compare_exchange_strong(desired, (int) pthread_self())) {
+        int myThreadId = (int) pthread_self();
+        while (!owner.compare_exchange_strong(desired, myThreadId)) {
             desired = NO_OWNER;
         }
     }
 
     void unlock() {
-        int desired = (int) pthread_self();
-        while (!owner.compare_exchange_strong(desired, NO_OWNER)) {
-            desired = (int) pthread_self();
-        }
+        assert(owner.load(std::memory_order_relaxed) == (int) pthread_self());
+        owner.store(NO_OWNER);
     }
 
 private:
@@ -40,72 +39,78 @@ private:
     atomic<int> owner;
 };
 
-const size_t THREAD_NUMBER = 4;
-const int MAX_GLOBAL = 100000000;
-
-void incrementer(Futex &futex, size_t &count, int &value, int maxValue) {
+template<typename T>
+void incrementer(T &locker, size_t &count, int &value, int maxValue) {
     while (true) {
-        futex.lock();
+        locker.lock();
         if (value < maxValue) {
             value++;
             ++count;
         } else {
-            futex.unlock();
+            locker.unlock();
             break;
         }
-        futex.unlock();
+        locker.unlock();
     }
 }
 
+template<typename T>
 size_t threadsCountToN(size_t threadNumber, int maxValue) {
     vector<std::thread> threads;
     vector<size_t> results(threadNumber);
-    Futex futex;
+    T locker;
     int global = 0;
 
     for (size_t i = 0; i < threadNumber; ++i) {
-        threads.push_back(
-                std::thread(incrementer, std::ref(futex), std::ref(results[i]), std::ref(global), maxValue)
-        );
+        threads.emplace_back(incrementer<T>, std::ref(locker), std::ref(results[i]), std::ref(global), maxValue);
     }
 
     for (size_t i = 0; i < threads.size(); ++i) {
         threads[i].join();
     }
+
     size_t operationCount = 0;
-    for (size_t c : results) {
-        operationCount += c;
-        cout << c << ' ';
+    for (size_t localCount : results) {
+        operationCount += localCount;
     }
-    cout << std::endl;
     return operationCount;
 }
 
-std::chrono::milliseconds measureSingleRun(size_t threadNumber) {
+template<typename T>
+std::chrono::milliseconds measureSingleRun(size_t threadNumber, int maxGlobal) {
     auto timeStart = std::chrono::steady_clock::now();
 
-    size_t  operationCount = threadsCountToN(threadNumber, MAX_GLOBAL);
+    size_t operationCount = threadsCountToN<T>(threadNumber, maxGlobal);
+    assert(operationCount == maxGlobal);
 
     auto timeStop = std::chrono::steady_clock::now();
-    auto workTime = std::chrono::milliseconds(std::chrono::duration_cast<std::chrono::milliseconds>(timeStop - timeStart).count());
+    auto workTime = std::chrono::milliseconds(
+            std::chrono::duration_cast<std::chrono::milliseconds>(timeStop - timeStart).count());
     cout << threadNumber << " threads " << operationCount << " in " << workTime.count() << "ms." << std::endl;
     return workTime;
 }
 
-std::chrono::milliseconds measureAverageTime(size_t threadNumber, size_t count) {
+template<typename T>
+void measureAverageTime(size_t threadNumber, size_t count) {
+    const int MAX_GLOBAL = 100000000;
     assert(count > 0);
     std::chrono::milliseconds sum(0);
     for (size_t i = 0; i < count; ++i) {
-        sum += measureSingleRun(threadNumber);
+        sum += measureSingleRun<T>(threadNumber, MAX_GLOBAL);
     }
     auto averageTime = sum / count;
     cout << "Average time for " << threadNumber << " threads is " << averageTime.count() << std::endl;
 }
 
 int main() {
-    measureAverageTime(thread::hardware_concurrency() / 4, 10); // 7601 ms (4275 ms)
-    measureAverageTime(thread::hardware_concurrency() / 2, 10); // 17724 ms (21920 ms)
-    measureAverageTime(thread::hardware_concurrency(), 5); // 28691 ms (18039)
-    measureAverageTime(thread::hardware_concurrency() * 2, 3); // 53792 ms (20773 ms)
+
+    measureSingleRun<Futex>(thread::hardware_concurrency() / 2, 400000000);
+    measureSingleRun<Futex>(thread::hardware_concurrency(), 400000000);
+    measureSingleRun<Futex>(thread::hardware_concurrency() * 2, 200000000);
+
+    measureSingleRun<std::mutex>(thread::hardware_concurrency() / 2, 400000000);
+    measureSingleRun<std::mutex>(thread::hardware_concurrency(), 400000000);
+    measureSingleRun<std::mutex>(thread::hardware_concurrency() * 2, 200000000);
+
     return 0;
 }
